1194bd90c6707cc77fd15d4efb6d89d9ee00bbbd
Astul Betizagasti <astul_95@hotmail.com>
2018-09-19 20:05:32 -0300

    Caballo listo

---
+++ Ajedrez/Assets/Scripts/PiecesControllers/CaballoController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaballoController : PieceController {
+
+    public override List<Vector3> LoadPosibleMoves()
+    {
+        List<Vector3> locations = new List<Vector3>();
+        for (int ejeXvar = -2; ejeXvar <= 2; ejeXvar++)
+        {
+            for (int ejeZvar = -2; ejeZvar <= 2; ejeZvar++)
+            {
+                Vector3 posToCheck = new Vector3(transform.position.x + ejeXvar, 0, transform.position.z + ejeZvar);
+
+                bool soloUnEjeEs2 = (Mathf.Abs(ejeXvar) == 2) ^ (Mathf.Abs(ejeZvar) == 2);
+                bool ningunEjeEs0 = (ejeXvar != 0) && (ejeZvar != 0);
+
+                if (soloUnEjeEs2 && ningunEjeEs0)
+                {
+                    if (gameController.CheckFree(posToCheck))
+                    {
+                        locations.Add(posToCheck);
+                    }                   
+                    else if (gameController.CheckOponent(posToCheck))
+                    {
+                        locations.Add(posToCheck);
+                    }
+                }
+            }
+        }
+        return locations;
+    }
+}
